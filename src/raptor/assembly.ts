9448c7058af45e36869b382ff58ac53cb3c86428
Sujan Parajuli <42593762+sjnprjl@users.noreply.github.com>
2025-04-02 02:29:32 -0400

    feat: add more classes

--- src/raptor/assembly.ts
+++ src/raptor/assembly.ts
@@ -1,11 +1,16 @@
-import { BinaryAssembly } from "../commonclasses";
-import { ICloneable, Int32, OBoolean } from "../types";
+import { BinaryAssembly, ObjectNull, ObjectString } from "../commonclasses";
+import { Byte, ICloneable, Int16, Int32, OBoolean } from "../types";
 
 abstract class BaseObject {
   objectId: number;
   constructor(objectId: number) {
     this.objectId = objectId;
   }
+}
+
+export class Ref<T> {
+  public ref!: T;
+  constructor(public mapId: Int32) {}
 }
 
 abstract class EnumBaseClass extends BaseObject {
@@ -31,7 +36,11 @@ export class Subchart_Kinds
 }
 export class FootPrint extends BaseObject implements ICloneable<FootPrint> {
   clone(): FootPrint {
-    throw new Error("Method not implemented.");
+    const footPrint = new FootPrint(this.objectId);
+    footPrint.left = this.left.clone();
+    footPrint.right = this.right.clone();
+    footPrint.height = this.height.clone();
+    return footPrint;
   }
   public left!: Int32;
   public right!: Int32;
@@ -43,21 +52,112 @@ export class Event_Kind
   implements ICloneable<Event_Kind>
 {
   clone(): Event_Kind {
-    throw new Error("Method not implemented.");
+    const kind = new Event_Kind(this.objectId);
+    kind.value__ = this.value__?.clone();
+    return kind;
   }
 }
 
 export class System_Guid extends BaseObject implements ICloneable<System_Guid> {
+  public _a!: Int32;
+  public _b!: Int16;
+  public _c!: Byte;
+  public _d!: Byte;
+  public _e!: Byte;
+  public _f!: Byte;
+  public _g!: Byte;
+  public _h!: Byte;
+  public _i!: Byte;
+  public _j!: Byte;
+  public _k!: Byte;
   clone(): System_Guid {
     const guid = new System_Guid(this.objectId);
+    guid._a = this._a.clone();
+    guid._b = this._b.clone();
+    guid._c = this._c.clone();
+    guid._d = this._d.clone();
+    guid._e = this._e.clone();
+    guid._f = this._f.clone();
+    guid._g = this._g.clone();
+    guid._h = this._h.clone();
+    guid._i = this._i.clone();
+    guid._j = this._j.clone();
+    guid._k = this._k.clone();
+
     return guid;
   }
 }
+
+export class Parallelogram
+  extends BaseObject
+  implements ICloneable<Parallelogram>
+{
+  _prompt!: ObjectString;
+  _is_input!: OBoolean;
+  _new_line!: OBoolean;
+  _input_expression!: OBoolean;
+  _serialization_version!: Int32;
+  _FP!: Ref<FootPrint>;
+  _text_str!: ObjectString;
+  _name!: ObjectString | Ref<ObjectString>;
+  _proximity!: Int32;
+  _head_height!: Int32;
+  _head_width!: Int32;
+  _head_heightOrig!: Int32;
+  _head_widthOrig!: Int32;
+  _connector_length!: Int32;
+  _x_location!: Int32;
+  _y_location!: Int32;
+  _Successor!: Ref<null>;
+  _parent!: ObjectNull | unknown;
+  _is_child!: OBoolean;
+  _is_beforeChild!: OBoolean;
+  _is_afterChild!: OBoolean;
+  _full_text!: OBoolean;
+  _height_of_text!: Int32;
+  _char_length!: Int32;
+  _rect!: System_Drawing_Rectangle | Ref<System_Drawing_Rectangle>;
+  _comment!: ObjectNull | unknown;
+  _created_guid!: System_Guid;
+  _changed_guid!: System_Guid;
+  clone(): Parallelogram {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export class IF_Control extends BaseObject implements ICloneable<IF_Control> {
+  clone(): IF_Control {
+    throw new Error("Method not implemented.");
+  }
+}
+
 export class Oval extends BaseObject implements ICloneable<Oval> {
+  _serialization_version!: Int32;
+  _text_str!: ObjectString;
+  _name!: ObjectString;
+  _proximity!: Int32;
+  _head_height!: Int32;
+  _head_heightOrig!: Int32;
+  _head_widthOrig!: Int32;
+  _connector_length!: Int32;
+  _x_location!: Int32;
+  _y_location!: Int32;
+  _Successor!: FootPrint;
+  _parent!: FootPrint | ObjectNull;
+  _is_child!: OBoolean;
+  _is_beforeChild!: OBoolean;
+  _is_afterChild!: OBoolean;
+  _full_text!: OBoolean;
+  _height_of_text!: Int32;
+  _char_length!: Int32;
+  _rect!: System_Drawing_Rectangle;
+  _comment!: ObjectNull | CommentBox | Ref<CommentBox>;
+  _created_guid!: System_Guid;
+  _changed_guid!: System_Guid;
+
   clone(): Oval {
     throw new Error("Method not implemented.");
   }
-  public _serialization_version!: Int32;
 }
 
 export class Logging_Info
@@ -79,13 +179,27 @@ export class System_Drawing_Rectangle
   public height!: Int32;
   clone(): System_Drawing_Rectangle {
     const rect = new System_Drawing_Rectangle(this.objectId);
-    rect.x = rect.x.clone();
-    rect.y = rect.y.clone();
-    rect.width = rect.width.clone();
-    rect.height = rect.height.clone();
+
+    rect.x = this.x.clone();
+    rect.y = this.y.clone();
+    rect.width = this.width.clone();
+    rect.height = this.height.clone();
     return rect;
   }
 }
+export class Rectangle extends BaseObject {}
+export class Rectangle_Kind_Of
+  extends EnumBaseClass
+  implements ICloneable<Rectangle_Kind_Of>
+{
+  clone(): Rectangle_Kind_Of {
+    const rect = new Rectangle_Kind_Of(this.objectId);
+    rect.value__ = this.value__;
+    return rect;
+  }
+}
+
+export class CommentBox extends BaseObject {}
 
 export const assembly = {
   0: {
@@ -100,15 +214,19 @@ export const assembly = {
       Subchart_Kinds: Subchart_Kinds,
       Oval: Oval,
       "Component+FootPrint": FootPrint,
-      Parallelogram: System_Drawing_Rectangle,
+      Parallelogram: Parallelogram,
       logging_info: Logging_Info,
       "logging_info+event_kind": Event_Kind,
+      IF_Control,
+      Rectangle,
+      "Rectangle+Kind_Of": Rectangle_Kind_Of,
+      CommentBox,
     },
   },
   3: {
     System: {
       Drawing: {
-        Rectangle: Object,
+        Rectangle: System_Drawing_Rectangle,
       },
     },
   },
